9d275f77214e7058f89dd5d1f5d60b4721412179
Daehyeon Kim <104838522@student.swin.edu.au>
2025-11-07 13:58:37 +1100

    edit path

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -9,15 +9,17 @@ from sklearn.exceptions import InconsistentVersionWarning
 warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
 
 try:
-    VECTORIZER = joblib.load("./models/vectorizer.joblib")
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    MODEL_DIR = os.path.join(BASE_DIR, "../../ml_models")
 
-    KNN_SCALER = joblib.load("./models/knn_scaler.joblib")
-    KNN = joblib.load("./models/knn_model.joblib")
-
-    RF_SCALER = joblib.load("./models/rf_scaler.joblib")
-    RF = joblib.load("./models/rf_model.joblib")
+    VECTORIZER = joblib.load(os.path.join(MODEL_DIR, "vectorizer.joblib"))
+    KNN_SCALER = joblib.load(os.path.join(MODEL_DIR, "knn_scaler.joblib"))
+    KNN = joblib.load(os.path.join(MODEL_DIR, "knn_model.joblib"))
+    RF_SCALER = joblib.load(os.path.join(MODEL_DIR, "rf_scaler.joblib"))
+    RF = joblib.load(os.path.join(MODEL_DIR, "rf_model.joblib"))
 
     print("Model components loaded successfully.")
+
 except Exception as e:
     print(f"Error loading model components: {e}")
     VECTORIZER = KNN_SCALER = RF_SCALER = KNN = RF = None
